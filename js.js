857342d086fbe670e83b135a5395a6cb253dfc9f
Sertaç Aytan <sertacayten2015@gmail.com>
2024-11-21 19:20:09 +0300

    güncelleme

--- js.js
+++ js.js
@@ -53,6 +53,7 @@ function onVideoEnd() {
         console.log("API Response:", data);  // Konsola gelen yanıtı yazdırın
         if (data.success) {
           console.log("Balance updated successfully.");
+          socket.close();
         } else {
           alert(`Error updating balance: ${data.message}`);
         }
@@ -62,7 +63,7 @@ function onVideoEnd() {
     console.error("Error retrieving Telegram ID:", error);
   }
   Telegram.WebApp.close();
-  socket.close();
+  
   
     
   
